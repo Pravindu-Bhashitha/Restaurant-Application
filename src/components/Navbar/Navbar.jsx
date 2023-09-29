9a80eb5b53f212a6bd0ab868bb6f0cee625bf5e6
Pravindu <pravindubh@gmail.com>
2023-09-29 12:37:21 +0530

    After finishing the navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { MdOutlineRestaurantMenu } from "react-icons/md";
 import images from "../../constants/images";
 import "./Navbar.css";
 
-const Navbar = () => (
+const Navbar = () => {
+  const [toggleMenu,setToggleMenu] = useState(false);
+  return (
   <nav className="app__navabar">
     <div className="app__navbar-logo">
       <img src={images.gericht} alt="app logo" />
@@ -36,14 +38,15 @@ const Navbar = () => (
       </a>
     </div>
     <div className="app__navbar-smallscreen">
-      <GiHamburgerMenu color="#fff" fontSize={27} onClick={() => {}} />
-      <div className="app__navbar-smallscren_overlay flex__center slide-bottom">
+      <GiHamburgerMenu color="#fff" fontSize={27} onClick={() => setToggleMenu(true)}/>
+      {toggleMenu && (
+      <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
         <MdOutlineRestaurantMenu
           fontSize={27}
           className="overlay__close"
-          onClick={() => {}}
+          onClick={() => {setToggleMenu(false)}}
         />
-        <ul className="app__navbar-smallscreen-links">
+        <ul className="app__navbar-smallscreen_links">
           <li className="p__opensans">
             <a href="#home">Home</a>
           </li>
@@ -61,8 +64,9 @@ const Navbar = () => (
           </li>
         </ul>
       </div>
+      )}
     </div>
   </nav>
-);
-
+)
+}
 export default Navbar;
